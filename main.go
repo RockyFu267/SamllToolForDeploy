4f27c05e099814aa8c0b91bfaaf2a6d552260c47
RockyFu267 <453618045@qq.com>
2022-02-18 16:04:56 +0800

    add 检查输出的目标目录

--- main.go
+++ main.go
@@ -14,7 +14,15 @@ func main() {
 	//获取参数
 	flag.Parse()
 	fmt.Println("hi buddy")
+	//获取所有文件列表
 	tgzFileList, err := basefunc.ReadFileListNew(*source_path)
+	if err != nil {
+		log.Println(err)
+		//***结束***
+		return
+	}
+	//检查目标输出路径
+	err = basefunc.CheckDir(*target_path)
 	if err != nil {
 		log.Println(err)
 		//***结束***
