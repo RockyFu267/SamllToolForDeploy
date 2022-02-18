e58ba1fabcbbb0a21371ae2df1ded9f08d724137
RockyFu267 <453618045@qq.com>
2022-02-18 16:00:11 +0800

    update test 创建目录

--- basefunc/checkDir_test.go
+++ basefunc/checkDir_test.go
@@ -1,23 +1,28 @@
 package basefunc
 
 import (
-	"fmt"
 	"log"
 	"testing"
 )
 
 func Test_CheckDir(t *testing.T) {
-	res, err := CheckDir("/Users/fuao/Downloads/chart-test/test01")
+	err := CheckDir("/Users/fuao/Downloads/chart-test/test01/002/0002/00002")
 	if err != nil {
 		log.Println("cmd.StdoutPipe: ", err)
 		return
 	}
-	fmt.Println(res)
-
 }
 
 func Test_CreateDir(t *testing.T) {
-	err := CreateDir("/Users/fuao/Downloads/chart-test/test01/asdasd")
+	err := CreateDir("/etc/fuao/Downloads/chart-test/test01/001/0001/000001/")
+	if err != nil {
+		log.Println("cmd.StdoutPipe: ", err)
+		return
+	}
+}
+
+func Test_CreateDirGoOS(t *testing.T) {
+	err := CreateDirGoOS("/Users/fuao/Downloads/chart-test/test01/002/0002/000002/")
 	if err != nil {
 		log.Println("cmd.StdoutPipe: ", err)
 		return
