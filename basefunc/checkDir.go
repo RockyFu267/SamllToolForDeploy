033748f2189161fda4aa310a106bd60fb94584fd
RockyFu267 <453618045@qq.com>
2022-02-18 15:55:38 +0800

    添加 创建路径 用CMD包

--- basefunc/checkDir.go
+++ basefunc/checkDir.go
@@ -1,27 +1,43 @@
 package basefunc
 
 import (
+	"SamllToolForDeploy/basecmd"
 	"log"
 	"os"
 )
 
-func CheckDir(path string) (bool, error) {
-	taget, err := os.Stat(path)
+func CheckDir(path string) error {
+	_, err := os.Stat(path)
 	if err != nil {
 		log.Println(err)
-		return false, err
+		// return  err
+		//如果不存在
+		err = CreateDir(path)
+		if err != nil {
+			return err
+		}
 	}
-	return taget.IsDir(), nil
+	return nil
 }
 
 //CreateDir path是绝对路径
-func CreateDir(path string) error {
-	err := os.Mkdir(path, 0777)
+func CreateDir(dir string) error {
+	_, err := basecmd.CmdAndChangeDirToResAllInOne("./", "mkdir -p "+dir)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+//CreateDirGoOS path是绝对路径,不支持多层目录创建，基于go的OS包
+func CreateDirGoOS(path string) error {
+	err := os.Mkdir(path, 0755)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 	//如果不赋权限会有问题
-	os.Chmod("path", 0777)
+	os.Chmod("path", 0755)
 	return nil
 }
