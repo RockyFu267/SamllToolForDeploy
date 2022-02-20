6d7b448f5825b8d70e98693565c3b35dfc9fbb25
RockyFu267 <453618045@qq.com>
2022-02-20 21:37:20 +0800

    add dryrun& 测试 目前的输出还未改成文件

--- basefunc/dryRun.go
+++ basefunc/dryRun.go
@@ -1,17 +1,38 @@
 package basefunc
 
 import (
+	"SamllToolForDeploy/basecmd"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 
 	yamlV2 "gopkg.in/yaml.v2"
 )
 
-func DryRun() error {
+//DryRun 通过helm dryrun 来获取渲染后的yaml
+func DryRun(targetpath string) error {
+	dirList, err := ReadDirList(targetpath)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	for _, v := range dirList {
+		chartNameTmp, err := GetChartName(targetpath + "/" + v + "/" + "Chart.yaml")
+		if err != nil {
+			continue
+		}
+		resTmp, err := basecmd.CmdAndChangeDirToResAllInOne(targetpath, "helm install -n testtest "+chartNameTmp+" ./"+v+" --dry-run")
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		fmt.Println(resTmp)
+	}
 	return nil
 }
 
+//GetChartName 获取chart包的name，过滤chart.yaml中name为空的包，需要用户手动填写准备
 func GetChartName(targetpath string) (string, error) {
 	var tmpStruct ChartYaml
 	yamlFile, err := ioutil.ReadFile(targetpath)
@@ -24,7 +45,7 @@ func GetChartName(targetpath string) (string, error) {
 		return "", err
 	}
 	if len(tmpStruct.Name) == 0 {
-		return "", errors.New("name is empty")
+		return "", errors.New("chart-name is empty")
 	}
 	return tmpStruct.Name, nil
 }
--- basefunc/dryRun_test.go
+++ basefunc/dryRun_test.go
@@ -6,6 +6,14 @@ import (
 	"testing"
 )
 
+func Test_DryRun(t *testing.T) {
+	err := DryRun("/Users/fuao/Downloads/chart-test/test01/test000/")
+	if err != nil {
+		log.Println("cmd.StdoutPipe: ", err)
+		return
+	}
+
+}
 func Test_GetChartName(t *testing.T) {
 	res, err := GetChartName("/Users/fuao/Downloads/chart-test/test01/test000/asd01/Chart.yaml")
 	if err != nil {
