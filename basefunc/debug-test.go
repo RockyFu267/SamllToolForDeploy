92bc567f0b6d8f3ae6ea164747981256932da80a
RockyFu267 <453618045@qq.com>
2022-02-25 23:56:10 +0800

    update

--- basefunc/debug-test.go
+++ basefunc/debug-test.go
@@ -7,33 +7,48 @@ import (
 	yamlV3 "gopkg.in/yaml.v3"
 )
 
-type StructA struct {
-	A string `yaml:"a"`
-}
+// type StructA struct {
+// 	A string `yaml:"a"`
+// }
 
-type StructB struct {
-	StructA
-	C StructA
-	B string `yaml:"b"`
-}
+// type StructB struct {
+// 	StructA
+// 	C StructA
+// 	B string `yaml:"b"`
+// }
+
+// var data = `
+// a: a string from struct A
+// b: a string from struct B
+// c:
+//   a: a string from c.a
+// `
 
 var data = `
-a: a string from struct A
-b: a string from struct B
-c:
-  a: a string from c.a
+aaa: aaa
+bbb: bbb
+ccc: 
+  cc1c: 
+    cc1c2c: cc1c2c
+# ddd: ddd
 `
 
-func DebugTestFunc() {
-	var b StructB
+//DebugTestFunc 测试
+func DebugTestFunc(path string) error {
+	var b TestTest
 	x := 22
 
 	err := yamlV3.Unmarshal([]byte(data), &b)
 	if err != nil {
 		log.Fatalf("cannot unmarshal data: %v", err)
+		return err
 	}
 
-	yfile, _ := os.Create("test.yaml")
+	yfile, err := os.Create(path)
+	if err != nil {
+		log.Println("cannot create data: ", err)
+		return err
+	}
 	defer yfile.Close()
 
 	yencoder := yamlV3.NewEncoder(yfile)
@@ -41,5 +56,6 @@ func DebugTestFunc() {
 	yencoder.Encode(b)
 	yencoder.Encode(b)
 	yencoder.Encode(x)
-	yencoder.Encode(44)
+	yencoder.Encode(444444)
+	return nil
 }
